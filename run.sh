dc82b3e87ae754691c2e1ef61a3eee6b03b5f30c
amirziai <arziai@gmail.com>
2017-12-18 11:23:19 -0800

    learning wget

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 conda install -y tqdm
-wget http://kitti.is.tue.mpg.de/kitti/data_road.zip -O /data/data_road.zip
+wget http://kitti.is.tue.mpg.de/kitti/data_road.zip -P data/
 unzip data/data_road.zip
 nohup python main.py > "run_$(date +"%Y_%m_%d_%I_%M_%p").log" 2>&1&
